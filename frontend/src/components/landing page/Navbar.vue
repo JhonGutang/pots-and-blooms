b611257135dccca7949790922d3b2feec2284214
Jhon Barry Gutang <jbgutang771@gmail.com>
2025-05-30 22:48:09 +0800

    adding horizontal padding on nav

--- frontend/src/components/landing page/Navbar.vue
+++ frontend/src/components/landing page/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="py-5 px-5 flex fixed top-0 justify-between w-[100vw] items-center z-99 bg-white">
+  <div class="py-5 px-10 flex fixed top-0 justify-between w-[100vw] items-center z-99 bg-white">
     <div class="text-3xl font-bold title">Pots and Blooms</div>
     <div class="flex">
       <router-link :to="navLink.link" v-for="(navLink, index ) in navlinks" :key="index" >
-        <v-btn variant="text">
+        <v-btn variant="text" size="small">
         {{ navLink.label }}
       </v-btn>
         
